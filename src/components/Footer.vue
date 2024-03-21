9d47d5fa09844a4c091de0bc59928166a6bd9541
Benedettopana <benedetto.pana@gmail.com>
2024-03-21 10:54:55 +0100

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+
+  methods: {
+    contatore() {
+      setInterval(() => {
+        this.counter++;
+      }, 1000);
+    },
+  },
+
+  mounted() {
+    console.log("footer montato");
+    this.contatore();
+  },
+};
+</script>
+
+<template>
+  <div>contatore: {{ counter }}</div>
+</template>
+
+<style scoped></style>
